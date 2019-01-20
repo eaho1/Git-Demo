e134ee869b813e5ef2b545ff3774e45e75e020b0
Edwin Ho <eaho555@gmail.com>
2019-01-19 23:47:07 -0800

    Update Player.cs

--- Git Demo/Assets/Scripts/Player.cs
+++ Git Demo/Assets/Scripts/Player.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
-    public float speed = 5f;
+    public float speed = 15f;
     
     // Start is called before the first frame update
     void Start()
